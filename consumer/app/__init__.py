95009d609cd038586e15d6e36ec54b94bd75e1d0
Mady0101 <mrabetmohamedmahdi@gmail.com>
2023-04-05 00:00:34 +0200

    consumer with mongo

--- consumer/app/__init__.py
+++ consumer/app/__init__.py
@@ -1,17 +1,19 @@
 import logging
-from json import loads
+import json
 import pymongo
 from app.enum import EnvironmentVariables as EnvVariables
 from app.dependecies.mongo import get_instance_db
+from json import loads
 
 from kafka import KafkaConsumer
 
 
 def main():
+    client = pymongo.MongoClient("mongodb://username:password@mongo:27017/mydatabase")
+    db = client["mydatabase"]
     try:
         # To consume latest messages and auto-commit offsets
-        # client = pymongo.MongoClient("mongodb://root:example@mongo:27017/")
-        # db = client["mydatabase"]
+        
         consumer = KafkaConsumer(
             EnvVariables.KAFKA_TOPIC_NAME.get_env(),
             bootstrap_servers=f'{EnvVariables.KAFKA_SERVER.get_env()}:{EnvVariables.KAFKA_PORT.get_env()}',
@@ -20,11 +22,13 @@ def main():
             enable_auto_commit=True,
         )
         for message in consumer:
-            print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                                 message.offset, message.key, message.value))
-            data = loads(message.value())
-            # result = db["recherche"].insert_one(data)
-            # print(result)
+            # print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+            #                                      message.offset, message.key, message.value))
+            print("message")
+            print(message.value)
+            
+            result = db["cities"].insert_one(message.value)
+            print(result)
 
     except Exception as e:
         logging.info('Connection successful', e)
