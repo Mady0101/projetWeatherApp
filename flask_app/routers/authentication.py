03b762a3238dd82100da4c49c1515ec1680c1338
Mady0101 <mrabetmohamedmahdi@gmail.com>
2023-04-08 06:08:40 +0200

    login w signup

--- flask_app/routers/authentication.py
+++ flask_app/routers/authentication.py
@@ -1,9 +1,13 @@
 import json
 import random
 from flask import (Blueprint,request,session,flash,redirect,url_for,render_template)
-
+from pymongo import MongoClient
 from flask_app.models.user import User
 
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client["mydatabase"]
+collection = db["users"]
 
 
 authentication_blueprint = Blueprint('authentication_blueprint', __name__)
@@ -17,22 +21,26 @@ def login():
         if not username or not password:
             flash('Please enter your username and password.')
             return redirect(url_for('.login'))
-        #get user from database or return null if not correct with status 200 
-
-        user = User(id=1, username='admin', password='admin',email='')
+        #get user from database or return null if not correct with status 200
         
-       
-        if user:
-            session['user'] = user.id
-            return redirect(url_for('home_blueprint.home' , user= user))
+        exist = collection.find_one({"username": username})
+        if exist:
+            if not exist.password == password:
+                flash('username or password are wrong')
+                return redirect(url_for('.login'))
+        if not exist:
+            flash('username or password are wrong')
+            return redirect(url_for('.login'))
+         
 
-        flash('Invalid username or password.')
-        return redirect(url_for('.login'))
+        user = User(exist.id, username=exist.username, password=exist.password,email=exist.email)
+        session['user'] = user.id
+        return redirect(url_for('home_blueprint.home' , user= user))
 
     return render_template('login.html')
 
 
-@authentication_blueprint.route('/signup')
+@authentication_blueprint.route('/signup' , methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
@@ -41,19 +49,23 @@ def signup():
 
         if not username or not password:
             flash('Please enter your username and password.')
-            return redirect(".signup")
+            return redirect(url_for('.signup'))
 
-        # send signup to backend and check 
-        # if any(user.username == username for user in users):
-        #     flash('Username already exists.')
-        #     return redirect(url_for('signup'))
+        exist = collection.find_one({"username": username})
+        print("exist")
+        print(exist)
+        if exist:
+            flash('username exist')
+            return redirect(url_for('.signup'))
+        
+        collection.insert_one({"username": username, 'password': password, 'email': email})
 
         user = User(id=random.randint(2,100), username=username, password=password, email=email)
         
         # add to database
         # users.append(user)
 
-        return redirect(url_for('profile'))
+        return redirect(url_for('home_blueprint.home'))
     return render_template('signup.html')
 
 @authentication_blueprint.route('/logout')
